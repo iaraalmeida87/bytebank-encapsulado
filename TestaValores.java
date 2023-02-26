760d1da56e271d09aecdeb6687a335e08af2fbbb
Iara Almeida <iara.almeida.profissional@gmail.com>
2023-02-26 11:57:41 -0300

    criando novas contas para testar o static

--- TestaValores.java
+++ TestaValores.java
@@ -2,6 +2,8 @@ public class TestaValores {
 
 	public static void main(String[] args) {
 		Conta conta = new Conta(8545, 19008);
+		Conta conta2 = new Conta(8545, 12345);
+		Conta conta3 = new Conta(4178, 890123);
 	}
 
 }
